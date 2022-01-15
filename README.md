78a88154b16930dba0645085e51c64e4facdb945
cloudtang <cloudtang@tencent.com>
2022-01-15 19:38:06 +0800

    [feat]: complete the exercise2 of catalog

--- README.md
+++ README.md
@@ -1,42 +1,27 @@
+# Background
+
+### Coarse To View
+* [6.830](http://db.lcs.mit.edu/6.830/assign.php)
+### Exercise To Complete
+* [lab1](https://github.com/MIT-DB-Class/simple-db-hw-2021/blob/master/lab1.md)
+* [lab2](https://github.com/MIT-DB-Class/simple-db-hw-2021/blob/master/lab2.md)
+* [lab3](https://github.com/MIT-DB-Class/simple-db-hw-2021/blob/master/lab3.md)
+* [lab4](https://github.com/MIT-DB-Class/simple-db-hw-2021/blob/master/lab4.md)
+* [lab5](https://github.com/MIT-DB-Class/simple-db-hw-2021/blob/master/lab5.md)
+* [lab6](https://github.com/MIT-DB-Class/simple-db-hw-2021/blob/master/lab6.md)
+
 # setup
 ### dependency
-junit-4.5.jar
-* http://junit.sourceforge.net/
-* CPL (free for all use)
-
-zql.jar
-* http://www.gibello.com/code/zql/
-* Free for non-commercial use
-
-JLine
-* http://jline.sourceforge.net/
-* BSD (free for all use)
-
-mina-core-2.0.4.jar
-mina-filter-compression-2.0.4.jar
-* http://mina.apache.org/
-* Apache License v2.0 (free for all use)
-
-slf4j-api-1.6.1.jar
-slf4j-log4j12-1.6.1.jar
-* http://www.slf4j.org/license.html
-* MIT license (free for all use)
-
-jzlib-1.0.7.jar
-* http://www.jcraft.com/jzlib/
-* BSD (free for all use)
-
-javassist-3.16.1-GA.jar
-* http://www.javassist.org/
-* MPL v1.1, LGPL and Apache License
-
-ant-contrib-1.0b3.jar
-* http://ant-contrib.sourceforge.net/
-* Apache Software License
-
-log4j-1.2.17.jar
-* logging.apache.org/log4j/1.2/
-* Apache Software license 2.0
+1. jdk
+2. maven
+### installation
+* mac
+  * brew install jdk maven
+* linux
+  * apt-get install jdk maven
+  * yum install jdk maven
+* windows
+  * hands on and good luck!
 
 # Architecture
 
--- easydb-storage/src/main/java/org/learn2pro/easydb/storage/Catalog.java
+++ easydb-storage/src/main/java/org/learn2pro/easydb/storage/Catalog.java
@@ -1,44 +1,73 @@
 package org.learn2pro.easydb.storage;
 
+import com.google.common.collect.Maps;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * The Catalog keeps track of all available tables in the database and their
- * associated schemas.
- * For now, this is a stub catalog that must be populated with tables by a
- * user program before it can be used -- eventually, this should be converted
- * to a catalog that reads a catalog table from disk.
+ * The Catalog keeps track of all available tables in the database and their associated schemas. For now, this is a stub
+ * catalog that must be populated with tables by a user program before it can be used -- eventually, this should be
+ * converted to a catalog that reads a catalog table from disk.
  *
  * @Threadsafe
  */
 public class Catalog {
 
     /**
-     * Constructor.
-     * Creates a new, empty catalog.
+     * id->db
+     */
+    private Map<Integer, DbFile> id2table;
+    /**
+     * name->id
+     */
+    private Map<String, Integer> name2id;
+    /**
+     * id->name
+     */
+    private Map<Integer, String> id2name;
+    /**
+     * id->key
+     */
+    private Map<Integer, String> id2key;
+    /**
+     * the id generator of table
+     */
+    private AtomicInteger idGenerator = new AtomicInteger(0);
+
+    /**
+     * Constructor. Creates a new, empty catalog.
      */
     public Catalog() {
-        // some code goes here
+        id2table = Maps.newConcurrentMap();
+        name2id = Maps.newConcurrentMap();
+        id2name = Maps.newConcurrentMap();
+        id2key = Maps.newConcurrentMap();
     }
 
     /**
-     * Add a new table to the catalog.
-     * This table's contents are stored in the specified DbFile.
-     * @param file the contents of the table to add;  file.getId() is the identfier of
-     *    this file/tupledesc param for the calls getTupleDesc and getFile
-     * @param name the name of the table -- may be an empty string.  May not be null.  If a name
-     * conflict exists, use the last table to be added as the table for a given name.
+     * Add a new table to the catalog. This table's contents are stored in the specified DbFile.
+     *
+     * @param file the contents of the table to add;  file.getId() is the identfier of this file/tupledesc param
+     *         for the calls getTupleDesc and getFile
+     * @param name the name of the table -- may be an empty string.  May not be null.  If a name conflict
+     *         exists, use the last table to be added as the table for a given name.
      * @param pkeyField the name of the primary key field
      */
     public void addTable(DbFile file, String name, String pkeyField) {
         // some code goes here
+        int id = file.getId();
+        id2table.put(id, file);
+        name2id.put(name, id);
+        id2name.put(id, name);
+        id2key.put(id, pkeyField);
     }
 
     public void addTable(DbFile file, String name) {
@@ -46,11 +75,11 @@ public class Catalog {
     }
 
     /**
-     * Add a new table to the catalog.
-     * This table has tuples formatted using the specified TupleDesc and its
-     * contents are stored in the specified DbFile.
-     * @param file the contents of the table to add;  file.getId() is the identfier of
-     *    this file/tupledesc param for the calls getTupleDesc and getFile
+     * Add a new table to the catalog. This table has tuples formatted using the specified TupleDesc and its contents
+     * are stored in the specified DbFile.
+     *
+     * @param file the contents of the table to add;  file.getId() is the identfier of this file/tupledesc param
+     *         for the calls getTupleDesc and getFile
      */
     public void addTable(DbFile file) {
         addTable(file, (UUID.randomUUID()).toString());
@@ -58,62 +87,70 @@ public class Catalog {
 
     /**
      * Return the id of the table with a specified name,
+     *
      * @throws NoSuchElementException if the table doesn't exist
      */
     public int getTableId(String name) throws NoSuchElementException {
         // some code goes here
-        return 0;
+        return getOrThrow(name2id, name);
     }
 
     /**
      * Returns the tuple descriptor (schema) of the specified table
-     * @param tableid The id of the table, as specified by the DbFile.getId()
-     *     function passed to addTable
+     *
+     * @param tableid The id of the table, as specified by the DbFile.getId() function passed to addTable
      * @throws NoSuchElementException if the table doesn't exist
      */
     public TupleDesc getTupleDesc(int tableid) throws NoSuchElementException {
         // some code goes here
-        return null;
+        return getOrThrow(id2table, tableid).getTupleDesc();
     }
 
     /**
-     * Returns the DbFile that can be used to read the contents of the
-     * specified table.
-     * @param tableid The id of the table, as specified by the DbFile.getId()
-     *     function passed to addTable
+     * Returns the DbFile that can be used to read the contents of the specified table.
+     *
+     * @param tableid The id of the table, as specified by the DbFile.getId() function passed to addTable
      */
     public DbFile getDatabaseFile(int tableid) throws NoSuchElementException {
         // some code goes here
-        return null;
+        return getOrThrow(id2table, tableid);
     }
 
     public String getPrimaryKey(int tableid) {
         // some code goes here
-        return null;
+        return getOrThrow(id2key, tableid);
     }
 
     public Iterator<Integer> tableIdIterator() {
         // some code goes here
-        return null;
+        return id2table.keySet().iterator();
     }
 
     public String getTableName(int id) {
         // some code goes here
-        return null;
+        return getOrThrow(id2name, id);
     }
 
-    /** Delete all tables from the catalog */
+    /**
+     * Delete all tables from the catalog
+     */
     public void clear() {
         // some code goes here
+        id2table.clear();
+        id2name.clear();
+        name2id.clear();
+        id2key.clear();
+        this.idGenerator = new AtomicInteger(0);
     }
 
     /**
      * Reads the schema from a file and creates the appropriate tables in the database.
+     *
      * @param catalogFile
      */
     public void loadSchema(String catalogFile) {
         String line = "";
-        String baseFolder=new File(new File(catalogFile).getAbsolutePath()).getParent();
+        String baseFolder = new File(new File(catalogFile).getAbsolutePath()).getParent();
         try {
             BufferedReader br = new BufferedReader(new FileReader(new File(catalogFile)));
 
@@ -129,18 +166,18 @@ public class Catalog {
                 for (String e : els) {
                     String[] els2 = e.trim().split(" ");
                     names.add(els2[0].trim());
-                    if (els2[1].trim().toLowerCase().equals("int"))
+                    if (els2[1].trim().toLowerCase().equals("int")) {
                         types.add(Type.INT_TYPE);
-                    else if (els2[1].trim().toLowerCase().equals("string"))
+                    } else if (els2[1].trim().toLowerCase().equals("string")) {
                         types.add(Type.STRING_TYPE);
-                    else {
+                    } else {
                         System.out.println("Unknown type " + els2[1]);
                         System.exit(0);
                     }
                     if (els2.length == 3) {
-                        if (els2[2].trim().equals("pk"))
+                        if (els2[2].trim().equals("pk")) {
                             primaryKey = els2[0].trim();
-                        else {
+                        } else {
                             System.out.println("Unknown annotation " + els2[2]);
                             System.exit(0);
                         }
@@ -149,16 +186,25 @@ public class Catalog {
                 Type[] typeAr = types.toArray(new Type[0]);
                 String[] namesAr = names.toArray(new String[0]);
                 TupleDesc t = new TupleDesc(typeAr, namesAr);
-                HeapFile tabHf = new HeapFile(new File(baseFolder+"/"+name + ".dat"), t);
-                addTable(tabHf,name,primaryKey);
+                HeapFile tabHf = new HeapFile(new File(baseFolder + "/" + name + ".dat"), t);
+                addTable(tabHf, name, primaryKey);
                 System.out.println("Added table : " + name + " with schema " + t);
             }
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(0);
         } catch (IndexOutOfBoundsException e) {
-            System.out.println ("Invalid catalog entry : " + line);
+            System.out.println("Invalid catalog entry : " + line);
             System.exit(0);
+        }
+    }
+
+
+    private <T> T getOrThrow(Map<?, T> holder, Object key) throws NoSuchElementException {
+        if (key != null && holder.containsKey(key)) {
+            return holder.get(key);
+        } else {
+            throw new NoSuchElementException(String.format("not found this key:%s", key));
         }
     }
 }
